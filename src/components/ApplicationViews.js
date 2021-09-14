1da4d14b54565625d23bbbde876af801aacec430
Ashton Monzi <anmonzi@gmail.com>
2021-09-14 15:00:40 -0500

    added luxon for time conversion, added new priority form added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom"
 import { Dashboard } from "./dashboard/Dashboard"
 import { DashboardProvider } from "./dashboard/DashboardProvider"
 import { PriorityProvider } from "./priorities/PriorityProvider"
-
+import { PriorityForm } from "./priorities/PriorityForm"
 
 export const ApplicationViews = () => {
     return <>
@@ -12,6 +12,10 @@ export const ApplicationViews = () => {
             <PriorityProvider>
                 <Route exact path="/dashboard">
                     <Dashboard />
+                </Route>
+
+                <Route exact path="/priority/new">
+                    <PriorityForm />
                 </Route>
             </PriorityProvider>
         </DashboardProvider>
